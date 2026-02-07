c9778e82297c59533a4e81f9da1492079bf7f48f
avigunjal07 <avishkargunjal07@gmail.com>
2026-02-07 14:33:41 +0530

    login modal is complete

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+function opanmodal(){
+    const overlayEle = document.getElementById('overlay');
+    overlayEle.style.display='flex';
+}
+
+function closemodal(){
+    const overlayEle = document.getElementById('overlay');
+    overlayEle.style.display='none';
+}
